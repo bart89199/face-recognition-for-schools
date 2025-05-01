b1b4f8ba82db20b54df6fa424f6f6201f2c29c80
danil <suslovds07@gmail.com>
2025-05-01 16:22:41 +0500

    GPU support and eyes move detection added

--- recogn.py
+++ recogn.py
@@ -1,3 +1,4 @@
+import threading
 import time
 
 import cv2
@@ -7,19 +8,20 @@ import pickle
 
 import numpy as np
 
-import google.auth
 from googleapiclient.http import MediaFileUpload
 import os.path
+from mediapipe.tasks.python import vision
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from mediapipe.tasks import python
 
 KNOWN_FACES_FILE = "known_faces.pkl"
 UNKNOWN_NAME = "unknown"
-MAX_DISTANCE = 0.5
+MAX_DISTANCE = 0.54
 # MIN_MATCH_FOR_PERSON = 0.3
 FRAME_SKIP = 3
 CAM_PORT = 0
@@ -29,14 +31,26 @@ TEMP_PATH = "tmp"
 last_saved_time = 0
 SAVE_DELAY = 7
 GOOGLE_DRIVE_FOLDER_ID = "1i-DoUmzkX9USiMLOOCeXjog52rma7RPW"
+SAVE_DETECTION_STATUS = False
 
 SCOPES = ["https://www.googleapis.com/auth/drive.file"]
 
-# Инициализация MediaPipe
-mp_face_detection = mp.solutions.face_detection
-mp_drawing = mp.solutions.drawing_utils
 
-face_detector = mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.7)
+# Инициализация MediaPipe
+base_options = python.BaseOptions(model_asset_path='blaze_face_short_range.tflite', delegate=python.BaseOptions.Delegate.GPU)
+face_detector_options = vision.FaceDetectorOptions(base_options=base_options)
+face_detector = vision.FaceDetector.create_from_options(face_detector_options)
+
+
+
+
+# mp_face_detection = mp.solutions.face_detection
+mp_face_mesh = mp.solutions.face_mesh
+# mp_drawing = mp.solutions.drawing_utils
+#
+# face_detector = mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.6)
+face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1, refine_landmarks=True)
+
 print("Starting video")
 cap = cv2.VideoCapture(CAM_PORT)
 print("Video started")
@@ -71,7 +85,33 @@ if not creds or not creds.valid:
         token.write(creds.to_json())
 
 
+# Параметры для моргания
+EAR_THRESHOLD = 0.2
+CONSEC_FRAMES = 2
+blink_counter = 0
+
+# Точки глаз в face_mesh (правый и левый глаз)
+RIGHT_EYE = [33, 160, 158, 133, 153, 144]
+LEFT_EYE = [362, 385, 387, 263, 373, 380]
+
+def eye_aspect_ratio(landmarks, eye_indices, img_width, img_height):
+    def _point(index):  # Перевод нормализованных координат в пиксели
+        p = landmarks[index]
+        return int(p.x * img_width), int(p.y * img_height)
+
+    p1, p2, p3, p4, p5, p6 = [_point(i) for i in eye_indices]
+    # Вертикальные расстояния
+    A = np.linalg.norm(np.array(p2) - np.array(p6))
+    B = np.linalg.norm(np.array(p3) - np.array(p5))
+    # Горизонтальное расстояние
+    C = np.linalg.norm(np.array(p1) - np.array(p4))
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+
 def save_frame(name, frame):
+    if not SAVE_DETECTION_STATUS:
+        return
     global last_saved_time
     current_time = time.time()
 
@@ -123,10 +163,29 @@ def show(frame, face_locations, face_names):
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
     for (_, _, _, _), name in zip(face_locations, face_names):
         if name != UNKNOWN_NAME:
-            save_frame(name, frame)
+            th = threading.Thread(target=save_frame, args = (name, frame))
+            th.start()
     cv2.imshow(WINDOW_NAME, frame)
 
+def check_eyes(frame, rgb_frame):
+    global blink_counter
+    results_mesh = face_mesh.process(rgb_frame)
+    h, w, _ = frame.shape
+    blinked = False
 
+    if results_mesh.multi_face_landmarks:
+        landmarks = results_mesh.multi_face_landmarks[0].landmark
+        left_ear = eye_aspect_ratio(landmarks, LEFT_EYE, w, h)
+        right_ear = eye_aspect_ratio(landmarks, RIGHT_EYE, w, h)
+        avg_ear = (left_ear + right_ear) / 2.0
+
+        if avg_ear < EAR_THRESHOLD:
+            blink_counter += 1
+        else:
+            if blink_counter >= CONSEC_FRAMES:
+                blinked = True
+            blink_counter = 0
+    return blinked
 def recognition(face_encoding):
     mx = [0, 0]
     for idx, known_faces in enumerate(known_face_encodings):
@@ -148,11 +207,17 @@ def get_locations(frame, results):
     if results.detections:
         h, w, _ = frame.shape
         for detection in results.detections:
-            box = detection.location_data.relative_bounding_box
-            top = int(box.ymin * h)
-            left = int(box.xmin * w)
-            bottom = top + int(box.height * h)
-            right = left + int(box.width * w)
+            # box = detection.bounding_box
+            # top = int(box.origin_y)
+            # left = int(box.xmin * w)
+            # bottom = top + int(box.height * h)
+            # right = left + int(box.width * w)
+
+            bbox = detection.bounding_box
+            left = bbox.origin_x
+            top = bbox.origin_y
+            right = bbox.origin_x + bbox.width
+            bottom = bbox.origin_y + bbox.height
 
             top = max(top, 0)
             left = max(left, 0)
@@ -168,7 +233,8 @@ def process(max_faces=10):
     if not ret:
         exit(6)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = face_detector.process(rgb_frame)
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    results = face_detector.detect(image=image)
 
     face_locations = get_locations(frame, results)
     while len(face_locations) > max_faces:
@@ -180,6 +246,14 @@ def process(max_faces=10):
     for face_encoding in face_encodings:
         name = recognition(face_encoding)
         face_names.append(name)
+
+    if not check_eyes(frame, rgb_frame):
+        cv2.putText(frame, "People not detected", (20, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1)
+    else:
+        cv2.putText(frame, "People detected", (20, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 1)
+
     show(frame, face_locations, face_names)
 
     return face_encodings
@@ -202,16 +276,6 @@ def save_data():
 
 print("[INFO] Нажмите 'U' чтобы сохранить лицо или обновить имеющееся, 'D' чтобы удалить лицо, 'Q' чтобы выйти.")
 while True:
-    # ret, frame = cap.read()
-    # if not ret:
-    #     exit(6)
-    # cv2.imshow("Распознавание лиц", frame)
-    # key = cv2.waitKey(1)
-    # if key == ord('q'):
-    #     break
-    # continue
-    # if frame_count % FRAME_SKIP != 0:
-    #  continue
     face_encodings = process()
     key = cv2.waitKey(1)
 
